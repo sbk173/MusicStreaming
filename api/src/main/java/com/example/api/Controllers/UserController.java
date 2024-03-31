cc362f068357306fbfa42c1f362d65096c8474fa
unknown <sathvik.vittal@gmail.com>
2024-03-31 19:49:58 +0530

    User done

---
+++ api/src/main/java/com/example/api/Controllers/UserController.java
@@ -0,0 +1,62 @@
+package com.example.api.Controllers;
+
+import com.example.api.Services.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+
+public class UserController{
+    final private UserService userService;
+
+    @Autowired
+    public UserController(UserController userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> RegUser(@RequestBody User user){
+        try{
+            userService.registerUser(user);
+            return new ResponseEntity<>(HttpStatus.CREATED); 
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @GetMapping("/profile/{userid}")
+    public ResponseEntity<?> getInfo(@PathVariable Long userid){
+        try{
+            User response = userService.getUser(userid);
+            return new ResponseEntity<>(response,HttpStatus.OK); 
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+    }
+
+    @GetMapping("login/{username}/{password}")
+    public ResponseEntity<?> login(@PathVariable("username") String user_name, @PathVariable("password") String password){
+        try{
+            User response = userService.login(user_name,password);
+            return new ResponseEntity<>(response,HttpStatus.OK); 
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+    }
+}
--- api/src/main/java/com/example/api/Services/UserService.java
+++ api/src/main/java/com/example/api/Services/UserService.java
@@ -9,8 +9,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
@@ -25,8 +23,57 @@ public class UserService {
         this.userRepo = userRepo;
     }
 
-    public void RegisterUser(User user){
+    public User registerUser(User user){
         // Fill here
+        String user_name = user.username;
+        Optional<User> temp = userRepo.findByUsername(user_name);
+        if (!temp.isPresent()){
+            try{
+                User response = userRepo.save(user);
+                return response;
+            }
+            catch(Exception e) {
+                throw new RuntimeException("Registration failed");
+            }
+        }
+
+        throw new RuntimeException("Username already exists");
+
+    }
+
+    public User login(String user_name, String pass){
+        // Fill here
+        try{
+            Optional<User> response = userRepo.findByUsername();
+            if (response.isPresent()){
+                User u = response.get();
+                if (u.password.equals(pass)){
+                    return u;
+                }
+            }
+            throw new RuntimeException("Incorrect username/password");
+            
+        }
+        catch(Exception e) {
+            throw new RuntimeException("Error logging in");
+        }
+
+    }
+
+    public User getUser(Long id){
+        try{
+            Optional<User> response = userRepo.findById(id);
+
+            if (response.isPresent()){
+
+                return response.get();
+            }
+            throw new RuntimeException("Login");
+        }
+        catch (Exception e){
+            throw new RuntimeException("Error");
+        }
         
     }
+
 }
